2f78b4111bd7382b021543a243db5ee2a8895265
Yazid <hazamiyazid7@gmail.com>
2026-05-26 22:19:52 +0700

    fix: correct imageContent path folder to match backend

--- app/src/main/java/com/example/myapplication/ui/screens/DetailPostScreen.kt
+++ app/src/main/java/com/example/myapplication/ui/screens/DetailPostScreen.kt
@@ -64,7 +64,7 @@ fun DetailPostScreen(postId: String, navController: NavController, viewModel: De
             ) {
                 Card(shape = RoundedCornerShape(12.dp)) {
                     AsyncImage(
-                        model = "${RetroFitClient.BASE_URL}uploads/imageContent/${post.thumbnail}",
+                        model = "${RetroFitClient.BASE_URL}uploads/posts/${post?.imageContent}",
                         contentDescription = null,
                         modifier = Modifier.fillMaxWidth().height(250.dp),
                         contentScale = ContentScale.Crop,
