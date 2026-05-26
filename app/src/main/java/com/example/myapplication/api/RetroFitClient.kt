ca6901a0c41e38de152498bfcc76d7d63c68e663
Yazid <hazamiyazid7@gmail.com>
2026-05-26 22:10:15 +0700

    fix: correct image url path in PostItem component

--- app/src/main/java/com/example/myapplication/api/RetroFitClient.kt
+++ app/src/main/java/com/example/myapplication/api/RetroFitClient.kt
@@ -9,6 +9,10 @@ object RetroFitClient {
 //  const val BASE_URL = "http://192.168.1.10:5000/"
     const val BASE_URL = "http://10.0.2.2:5079/"
 
+    const val THUMBNAIL_URL = "${BASE_URL}uploads/thumbnails/"
+    const val POST_IMAGE_URL = "${BASE_URL}uploads/posts/"
+    const val USER_PHOTO_URL = "${BASE_URL}uploads/users/"
+
     val instance: ApiService by lazy{
         Retrofit.Builder()
             .baseUrl(BASE_URL)
