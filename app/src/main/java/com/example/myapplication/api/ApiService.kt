204513f46a57fe3e7f93ed15486cea865e5fb0c2
Yazid <hazamiyazid7@gmail.com>
2026-05-27 21:01:59 +0700

    feat: add searchPosts to ApiService

--- app/src/main/java/com/example/myapplication/api/ApiService.kt
+++ app/src/main/java/com/example/myapplication/api/ApiService.kt
@@ -25,6 +25,7 @@ import retrofit2.http.POST
 import retrofit2.http.PUT
 import retrofit2.http.Part
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface ApiService{
 
@@ -153,4 +154,7 @@ interface ApiService{
 
     @GET("api/Users/{id}")
     suspend fun getUserById(@Path("id") id: String): User
+
+    @GET("api/Posts/search")
+    suspend fun searchPosts(@Query("query") query: String): List<Post>
 }
