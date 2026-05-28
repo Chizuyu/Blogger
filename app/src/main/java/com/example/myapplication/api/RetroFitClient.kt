4ec5e371977e1b6b0023fe12af29fe18827f24fc
Yazid <hazamiyazid7@gmail.com>
2026-05-28 13:06:19 +0700

    chore: revert RetroFitClient to original configuration without interceptors

--- app/src/main/java/com/example/myapplication/api/RetroFitClient.kt
+++ app/src/main/java/com/example/myapplication/api/RetroFitClient.kt
@@ -8,24 +8,11 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetroFitClient {
 //  const val BASE_URL = "http://10.0.2.2:5000/"
 //  const val BASE_URL = "http://192.168.1.7:5079/"
-    const val BASE_URL = "http://10.0.2.2:5079/"
-
-    private val client = OkHttpClient.Builder().addInterceptor { chain ->
-        val requestBuilder = chain.request().newBuilder()
-
-        val token = GlobalData.tokenUser
-
-        if (!token.isNullOrEmpty()) {
-            requestBuilder.addHeader("Authorization", "Bearer $token")
-        }
-
-        chain.proceed(requestBuilder.build())
-    }.build()
+const val BASE_URL = "http://192.168.1.7:5079/"
 
     val instance: ApiService by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
-            .client(client) // Pasang Satpamnya di sini
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(ApiService::class.java)
